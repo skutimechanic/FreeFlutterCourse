8fda805070bdd81492482e271bac6f2a6c8704e9
skutimechanic <koszowskimtb@gmail.com>
2023-01-05 16:33:27 +0100

    step-14

--- mynotes/lib/views/notes/notes_view.dart
+++ mynotes/lib/views/notes/notes_view.dart
@@ -76,7 +76,9 @@ class _NotesViewState extends State<NotesView> {
                 stream: _notesService.allNotes,
                 builder: (context, snapshot) {
                   switch (snapshot.connectionState) {
+                    // fall throw
                     case ConnectionState.waiting:
+                    case ConnectionState.active:
                       return const Text('Waiting for all notes...');
                     default:
                       return const CircularProgressIndicator();
