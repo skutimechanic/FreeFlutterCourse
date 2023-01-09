6fd993514faa30b3d6b321349ed28964d8ae682c
skutimechanic <koszowskimtb@gmail.com>
2023-01-09 22:13:13 +0100

    step-16

---
+++ mynotes/lib/utilities/dialogs/delete_dialog.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
+
+Future<bool> showDeleteDialog(BuildContext context) {
+  return showGenericDialog<bool>(
+    context: context,
+    title: 'Delete',
+    content: 'Are you sure you want to delete this item?',
+    optionsBuilder: () => {
+      'Cancel': false,
+      'Yes': true,
+    },
+    // to handle click outside the dialog - defining default behavior
+  ).then(
+    (value) => value ?? false,
+  );
+}
