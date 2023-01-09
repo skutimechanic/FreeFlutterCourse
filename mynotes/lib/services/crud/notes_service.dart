06fb97618ca4e7f507fc267d711bbd58387837a6
skutimechanic <koszowskimtb@gmail.com>
2023-01-09 23:36:56 +0100

    fixing update and removing import

--- mynotes/lib/services/crud/notes_service.dart
+++ mynotes/lib/services/crud/notes_service.dart
@@ -56,10 +56,15 @@ class NotesService {
     await getNote(id: note.id);
 
     // update DB
-    final updatesCount = await db.update(noteTable, {
-      textColumn: text,
-      isSyncedWithCloudColumn: 0,
-    });
+    final updatesCount = await db.update(
+      noteTable,
+      {
+        textColumn: text,
+        isSyncedWithCloudColumn: 0,
+      },
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
 
     if (updatesCount == 0) {
       throw CouldNotUpdateNoteException();
