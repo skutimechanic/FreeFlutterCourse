3739dfd04d046b2ee22132f4bf8699de30b29275
skutimechanic <koszowskimtb@gmail.com>
2023-01-10 19:11:24 +0100

    step-19

--- mynotes/lib/services/auth/auth_user.dart
+++ mynotes/lib/services/auth/auth_user.dart
@@ -3,17 +3,20 @@ import 'package:flutter/cupertino.dart';
 
 @immutable // will not change after initialization
 class AuthUser {
-  final String? email;
+  final String id;
+  final String email;
   final bool isEmailVerified;
 
 // required sets field to be assign with naming
   const AuthUser({
+    required this.id,
     required this.email,
     required this.isEmailVerified,
   });
 
   factory AuthUser.fromFirebase(User user) => AuthUser(
-        email: user.email,
+        id: user.uid,
+        email: user.email!,
         isEmailVerified: user.emailVerified,
       );
 }
