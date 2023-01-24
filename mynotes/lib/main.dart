7d92bcfba987aada8bdf27250ccb31b5ddd3dd7f
skutimechanic <koszowskimtb@gmail.com>
2023-01-24 18:27:14 +0100

    step-28

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -16,6 +16,7 @@ import 'package:mynotes/views/verify_email_view.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: 'Flutter Demo',
     theme: ThemeData(
       primarySwatch: Colors.blue,
